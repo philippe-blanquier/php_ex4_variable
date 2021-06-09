df48ac3d6a55ee103b34b2bcd86495026e45c7c1
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:14:24 +0200

    evo

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 	<head>
         <title>PHP ex4</title>
     </head>
-    <body >
-        <?php
-        $v_string="string";
-        $v_int=1;
-        $v_float=3.14;
-        $v_boolean=TRUE;
-        echo nl2br("v_string=".$v_string."\nv_int=".$v_int."\nv_float=".$v_float."\nv_boolean=".$v_boolean);
-        ?>
+    <body>
+        <p>
+            <?php
+            $v_string="string";
+            $v_int=1;
+            $v_float=3.14;
+            $v_boolean=TRUE;
+            echo nl2br("v_string=".$v_string."\nv_int=".$v_int."\nv_float=".$v_float."\nv_boolean=".$v_boolean);
+            ?>
+        </p>
     </body>
 </html>
