b5f887848c0a6a32a72b2c03f8492cbb848d5a72
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 10:47:44 +0200

    creation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex4</title>
+    </head>
+    <body >
+        <?php
+        $v_string="string";
+        $v_int=1;
+        $v_float=3.14;
+        $v_boolean=TRUE;
+        echo nl2br("v_string=".$v_string."\nv_int=".$v_int."\nv_float=".$v_float."\nv_boolean=".$v_boolean);
+        ?>
+    </body>
+</html>
